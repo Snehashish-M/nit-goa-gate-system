f8be2bb5e78042844678776082f003536eecda94
Snehashish-M <snehashishmandal2004@gmail.com>
2026-04-04 22:22:27 +0530

    Rename app to Dvarika, update launcher icon to 11.png

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NIT Goa Gate System',
+      title: 'Dvarika',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: const Color(0xFF0A192F),
